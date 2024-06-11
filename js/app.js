f9af48186c4469d607198d23b9d03421cc12bf51
jasontgomez <jason.gomez@mygeorgian.ca>
2024-06-11 19:18:15 -0400

    info
    added student info

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 // Assignment 1 | COMP1073 Client-Side JavaScript
+// Jason Gomez 200587201
+// Daniel Perusse 20055190
+// Sebastian 200561191
+// COMP1073
 
 /* Variables
 -------------------------------------------------- */
